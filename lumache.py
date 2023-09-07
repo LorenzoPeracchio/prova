c870098196356f884c1b3fd3bc27d6c8a5d57f1a
LorenzoPeracchio <124535474+LorenzoPeracchio@users.noreply.github.com>
2023-09-07 18:04:42 +0200

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -21,3 +21,20 @@ def get_random_ingredients(kind=None):
     :rtype: list[str]
     """
     return ["shells", "gorgonzola", "parsley"]
+
+def create_autoencoder(layer_sizes):
+    """
+    Gets an autoencoder model with the specified sizes of the layers.
+
+    This function gets an autoencoder model using the `AE` class, implemented as a PyTorch module, with the specified
+    layers' sizes.
+    The autoencoder is used for the implementation of the Density Principle.
+
+    :param list layer_sizes: A list containing the number of nodes of each layer of the encoder (decoder built with
+     symmetry).
+
+    :return: An instance of the autoencoder model.
+    :rtype: ReliabilityClasses.AE
+    """
+    ae = AE(layer_sizes)
+    return ae
